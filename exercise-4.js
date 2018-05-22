5427fd503ecd2f9c1c2a99cafa824a986303792c
Hendi Ivan Mahendro <hendivanm.him@gmail.com>
2018-05-22 13:11:13 +0700

    Berlatih Switch

---
+++ exercise-4.js
@@ -0,0 +1,18 @@
+var hari = 19;
+var bulan = 3;
+var tahun = 2018;
+
+switch (bulan) {
+    case "januari" : console.log (hari + " Januari " + tahun); break;
+    case "februari" : console.log (hari + " Februari " + tahun); break;
+    case "maret" : console.log (hari + " Maret " + tahun); break;
+    case "april" : console.log (hari + " April " + tahun); break;
+    case "mei" : console.log (hari + " Mei " + tahun); break;
+    case "juni" : console.log (hari + " Juni " + tahun); break;
+    case "juli" : console.log (hari + " Juli " + tahun); break;
+    case "agustus" : console.log (hari + " Agustus " + tahun); break;
+    case "september" : console.log (hari + " September " + tahun); break;
+    case "oktober" : console.log (hari + " Oktober " + tahun); break;
+    case "november" : console.log (hari + " November " + tahun); break;
+    case "desember" : console.log (hari + " Desember " + tahun); break;
+}
