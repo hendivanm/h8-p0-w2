8b8673459d7fb0df2e2088958d66da65ec52b781
Hendi Ivan Mahendro <hendivanm.him@gmail.com>
2018-05-24 06:01:26 +0700

    Tugas 7

---
+++ exercise7.js
@@ -0,0 +1,31 @@
+/*
+1. Menyusun Barisan Bintang
+===========================
+*/
+for (var rows1 = 1; rows1 <= 5; rows1++) {
+    console.log ("*");
+}
+
+/*
+2. Menyusun Barisan Bintang Dengan Nested Looping
+=================================================
+*/
+var rows2 = 6;
+var star = "";
+for (var rows3 = 1; rows3 < rows2; rows3++) {
+    for (var rows4 = 1; rows4 < rows2; rows4++) {
+    star += "*";
+}
+    console.log (star);
+    star = "";
+}
+
+/*
+3. Menyusun Barisan Tangga Bintang Dengan Nested Looping
+========================================================
+*/
+var star2 = "*";
+for (var rows5 = 1; rows5 <= 5; rows5++) {
+    console.log (star2);
+    star2 += "*";
+}
