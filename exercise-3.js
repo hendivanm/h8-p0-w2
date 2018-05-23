1277a4885b5d9d3f974adb90a60585c09e097d9f
Hendi Ivan Mahendro <hendivanm.him@gmail.com>
2018-05-23 13:54:38 +0700

    Memperbaiki Tugas 3

--- exercise-3.js
+++ exercise-3.js
@@ -1,25 +1,25 @@
-var nama = "Jason"; "Zack"; "Billy"; "Kimberly";
-var peran = "Ksatria"; "Tabib"; "Penyihir";
+var nama = "Ivan";
+var peran = "Tabib";
 
 if (nama === "" && peran === "") {
     console.log ("Nama harus diisi!");
-}
-
-if (nama === "Jason" && peran === "") {
-    console.log ("Halo Jason, Pilih peranmu untuk memulai game!");
 } 
 
-if (nama === "Zack" && peran === "Ksatria") {
-    console.log ("Selamat datang di Dunia Proxytia, Zack");
-    console.log ("Halo Ksatria Zack, kamu dapat menyerang dengan senjatamu!");
+else if (nama !== "" && peran === "") {
+    console.log ("Halo " + nama + " Pilih peranmu untuk memulai game!");
+} 
+
+else if (nama !== "" && peran === "Ksatria") {
+    console.log ("Selamat datang di Dunia Proxytia " + nama);
+    console.log ("Halo " + peran + " " + nama + ", kamu dapat menyerang dengan senjatamu!");
 }
 
-if (nama === "Billy" && peran === "Tabib") {
-    console.log ("Selamat datang di Dunia Proxytia, Billy");
-    console.log ("Halo Tabib Billy, kamu akan membantu temanmu yang terluka.");
+else if (nama !== "" && peran === "Tabib") {
+    console.log ("Selamat datang di Dunia Proxytia " + nama);
+    console.log ("Halo " + peran + " " + nama + ", kamu akan membantu temanmu yang terluka");
 }
 
-if (nama === "Kimberly" && peran === "Penyihir") {
-    console.log ("Selamat datang di Dunia Proxytia, Kimberly");
-    console.log ("Halo Penyihir Kimberly, ciptakan keajaiban yang membantu kemenanganmu!");
+else if (nama !== "" && peran === "Penyihir") {
+    console.log ("Selamat datang di Dunia Proxytia " + nama);
+    console.log ("Halo " + peran + " " + nama + ", ciptakan keajaiban yang membantu kemenanganmu!")
 }
