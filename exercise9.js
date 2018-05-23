82beb1813eadd58c980c28f918dec9582cb5547b
Hendi Ivan Mahendro <hendivanm.him@gmail.com>
2018-05-24 06:10:53 +0700

    Berlatih Penggunaan Functions

---
+++ exercise9.js
@@ -0,0 +1,40 @@
+/*
+Tugas 1
+=======
+*/
+function shoutOut () {
+    return "Halo Function!";
+}
+
+console.log (shoutOut())
+
+/*
+Tugas 2
+=======
+*/
+function calculateMultiply (num1, num2) {
+    return num1 * num2;
+}
+
+var num1 = 90;
+var num2 = 80;
+
+var hasilPerkalian = calculateMultiply (num1, num2);
+console.log (hasilPerkalian);
+
+/*
+Tugas 3
+=======
+*/
+function processSentence (name, age, address, hobby) {
+    var fullSentence = "Nama saya " + name + ", umur saya " + age + " tahun, alamat saya di " + address + ", dan saya punya hobby yaitu " + hobby +"!";
+    return fullSentence;
+}
+
+var name = "Ivan";
+var age = 23;
+var address = "Jakarta Selatan";
+var hobby = "menggambar kartun";
+
+var fullSentence = processSentence (name, age, address, hobby);
+console.log (fullSentence);
