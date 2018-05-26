b39551bde54f4d62c0d641a6ca898a79a0ad0940
Hendi Ivan Mahendro <hendivanm.him@gmail.com>
2018-05-26 11:12:13 +0700

    Mengenal penggunaan Conditional if-else dalam JavaScript

--- exercise-3.js
+++ exercise-3.js
@@ -1,25 +1,28 @@
 var nama = "Ivan";
-var peran = "Tabib";
+var peran = "";
 
 if (nama === "" && peran === "") {
     console.log ("Nama harus diisi!");
 } 
 
-else if (nama !== "" && peran === "") {
+else if (peran === "") {
     console.log ("Halo " + nama + " Pilih peranmu untuk memulai game!");
 } 
 
-else if (nama !== "" && peran === "Ksatria") {
+else if (peran === "Ksatria") {
     console.log ("Selamat datang di Dunia Proxytia " + nama);
     console.log ("Halo " + peran + " " + nama + ", kamu dapat menyerang dengan senjatamu!");
 }
 
-else if (nama !== "" && peran === "Tabib") {
+else if (peran === "Tabib") {
     console.log ("Selamat datang di Dunia Proxytia " + nama);
     console.log ("Halo " + peran + " " + nama + ", kamu akan membantu temanmu yang terluka");
 }
 
-else if (nama !== "" && peran === "Penyihir") {
+else if (peran === "Penyihir") {
     console.log ("Selamat datang di Dunia Proxytia " + nama);
     console.log ("Halo " + peran + " " + nama + ", ciptakan keajaiban yang membantu kemenanganmu!")
+}
+else {
+    console.log ("Maaf " + nama + ", fitur belum tersedia");
 }
